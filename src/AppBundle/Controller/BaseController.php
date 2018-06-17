3c7cf2d60549decf16e540e0b01b35da42328377
KilinSight <Kilinsight@mail.ru>
2018-06-17 14:48:47 +0700

    npm

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -2,17 +2,19 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class BaseController extends Controller
 {
 
     /**
-     * return EntityManager
+     * @return EntityManager
      */
     public function getEm()
     {
@@ -20,7 +22,16 @@ class BaseController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         return $em;
+    }
 
+    /**
+     * @return User
+     */
+    public function getCurrentUser()
+    {
+        $usr= $this->get('security.token_storage')->getToken()->getUser();
+
+        return $usr;
     }
 
 }
